dce2693541cb168b88c21096df804cf8dcfd0a2b
Feek18 <bintangfikri31@gmail.com>
2024-07-05 21:08:55 +0800

    push regis

--- Frontend/src/components/Fragments/FormRegister.jsx
+++ Frontend/src/components/Fragments/FormRegister.jsx
@@ -1,43 +1,75 @@
-import React from 'react'
-import InputForm from '../Elements/Input/Index'
-import Button from '../Elements/Button/Button'
+import React, { useEffect, useState } from "react";
+import InputForm from "../Elements/Input/Index";
+import Button from "../Elements/Button/Button";
+// import { useHistory } from 'react-router-dom';
 
 const FormRegister = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:4000/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (response.ok) {
+        setFormData({
+          username: "",
+          email: "",
+          password: "",
+        });
+        alert("Register Success");
+      } else {
+        alert(data.message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <InputForm
         label="Username"
         type="text"
         placeholder="Lowkey is Here"
         name="username"
+        value={formData.username}
+        onChange={handleChange}
       />
       <InputForm
         label="Email"
         type="email"
         placeholder="example@gmail.com"
         name="email"
+        value={formData.email}
+        onChange={handleChange}
       />
       <InputForm
         label="Password"
         type="password"
         placeholder="******"
         name="password"
+        value={formData.password}
+        onChange={handleChange}
       />
-      <InputForm
-        label="Konfirm Password"
-        type="password"
-        placeholder="******"
-        name="confirm_password"
-      />
-      <Button
-        color="bg-[#B2AFE7]"
-        px="6"
-        py="2"
-        text="Daftar"
-        width="w-full"
-      />
+      <Button color="bg-[#B2AFE7]" px="6" py="2" text="Daftar" width="w-full" />
     </form>
-  )
-}
+  );
+};
 
-export default FormRegister
+export default FormRegister;
