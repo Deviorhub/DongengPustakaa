5543d2ea06b1067d6e34a2237f0142d9af925795
Feek18 <bintangfikri31@gmail.com>
2024-07-07 13:48:13 +0800

    update responsive dashboard

--- Frontend/src/dashboard/Dashboard.jsx
+++ Frontend/src/dashboard/Dashboard.jsx
@@ -61,8 +61,8 @@ const Dashboard = () => {
     <>
       <Header />
       <div className="container mx-auto mt-10 px-20">
-        <div className="flex items-center justify-center gap-x-6">
-          <div className="flex justify-center items-center gap-x-4 border border-gray-300 rounded-md px-3 w-3/5">
+        <div className="lg:flex items-center justify-center  lg:gap-x-6">
+          <div className="flex justify-center items-center gap-x-4 border border-gray-300 rounded-md px-3 lg:w-3/5">
             <input
               type="text"
               className="flex-1 outline-none border-none"
@@ -72,37 +72,39 @@ const Dashboard = () => {
             />
             <SearchIcon className="h-5 w-5 text-gray-500 mr-2" />
           </div>
-          <Dropdown
-            label={
-              <div className="flex items-center text-[#282828]">
-                <span className="mr-2">Kategori</span>
-                <ChevronDownIcon className="w-5 h-5" />
-              </div>
-            }
-            dismissOnClick={false}
-          >
-            <Dropdown.Item>Cerita Rakyat</Dropdown.Item>
-            <Dropdown.Item>Kisah Dongeng</Dropdown.Item>
-          </Dropdown>
-          <Dropdown
-            label={
-              <div className="flex items-center text-[#282828]">
-                <span className="mr-2">Rating</span>
-                <ChevronDownIcon className="w-5 h-5" />
-              </div>
-            }
-            dismissOnClick={false}
-          >
-            <Dropdown.Item>
-              <StarIcon sx={{ color: "yellow" }} className="pr-3" />5 - 4
-            </Dropdown.Item>
-            <Dropdown.Item>
-              <StarIcon sx={{ color: "yellow" }} className="pr-3" />3 - 1
-            </Dropdown.Item>
-          </Dropdown>
+          <div className="flex justify-end items-center gap-x-4 mt-2">
+            <Dropdown
+              label={
+                <div className="flex items-center text-[#282828] px-3">
+                  <span className="mr-2">Kategori</span>
+                  <ChevronDownIcon className="w-5 h-5" />
+                </div>
+              }
+              dismissOnClick={false}
+            >
+              <Dropdown.Item className="p-4">Cerita Rakyat</Dropdown.Item>
+              <Dropdown.Item className="p-4">Kisah Dongeng</Dropdown.Item>
+            </Dropdown>
+            <Dropdown
+              label={
+                <div className="flex items-center text-[#282828] px-3">
+                  <span className="mr-2">Rating</span>
+                  <ChevronDownIcon className="w-5 h-5" />
+                </div>
+              }
+              dismissOnClick={false}
+            >
+              <Dropdown.Item>
+                <StarIcon sx={{ color: "yellow" }} className="p-4" />5 - 4
+              </Dropdown.Item>
+              <Dropdown.Item>
+                <StarIcon sx={{ color: "yellow" }} className="p-4" />3 - 1
+              </Dropdown.Item>
+            </Dropdown>
+          </div>
         </div>
       </div>
-      <div className="flex flex-wrap justify-between items-center container mx-auto px-20 py-10">
+      <div className="grid grid-cols-1 md:grid md:grid-cols-3 lg:grid lg:grid-cols-6 px-20 py-10">
         {datas.map((data, index) => (
           <Link key={index} to={`detail/${data.id}`} className="cursor-pointer">
             <Card judul={data.judul} img={data.image} rating={data.rating} />
