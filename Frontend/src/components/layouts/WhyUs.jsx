f7be6cfb7f2e4ed1ce3ed609c6545629ad0c2c2e
Feek18 <bintangfikri31@gmail.com>
2024-07-08 00:53:06 +0800

    update

--- Frontend/src/components/layouts/WhyUs.jsx
+++ Frontend/src/components/layouts/WhyUs.jsx
@@ -7,8 +7,7 @@ const WhyUs = () => {
         <div>
           <h1 className="text-4xl font-bold mb-2">Kenapa Kami?</h1>
           <p className="text-[18px] max-w-2xl">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt
-            reprehenderit cumque sint eos ipsam officiis?
+            Adapun beberapa keuntungan yang kami berikan pada anda
           </p>
           <div className="flex flex-col justify-center items-center lg:grid md:grid-cols-2 lg:grid-cols-3 gap-6 mt-10">
             <div className="bg-[#F2F2F2] rounded-md w-80 lg:w-96">
@@ -18,7 +17,7 @@ const WhyUs = () => {
                 className="w-full h-48 object-cover rounded-t-md"
               />
               <div className="p-4">
-                <h3 className="text-xl font-bold mb-2">Lorem, ipsum dolor.</h3>
+                <h3 className="text-xl font-bold mb-2">Cerita Terbaru</h3>
                 <p className="text-gray-600">Lorem ipsum dolor sit amet.</p>
               </div>
             </div>
